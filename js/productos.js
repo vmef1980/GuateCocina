5b4aa63e5ae7eacb4a0aa91325ff0e51936b6128
vmef1980 <99444715+vmef1980@users.noreply.github.com>
2025-11-10 03:11:49 -0600

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,9 +1,10 @@
-export const productos = await fetch("GuateCocina-v2.json")
+export const productos = await fetch("data/GuateCocina-v2.json")
   .then(res => res.json())
   .catch(err => {
     console.error("Error al cargar inventario:", err);
     return [];
   });
+
 
 
 
